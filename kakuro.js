eb0a7f1e8c4df5e85be2c1fd7e6c26dde66af1c3
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-12-14 09:53:18 +0000

    Get most of Kakuro solving machinery in place

--- kakuro.js
+++ kakuro.js
@@ -1,5 +1,37 @@
 const { Constraint, CSP } = require('./csp')
 
+const sum = xs => xs.reduce((a, b) => a + b, 0)
+
+const makeVariable = (row, col) => `${row}:${col}`
+
+class KakuroConstraint extends Constraint {
+
+  constructor(variables, sum) {
+    super(variables)
+    this.sum = sum
+  }
+
+  satisfied(assignment) {
+    const values = this.variables.flatMap(variable =>
+      assignment.has(variable) ? [assignment.get(variable)] : [])
+    const set = new Set([values])
+    if (set.size !== values.length) {
+      return false
+    }
+    const sumOfValues = sum(values)
+    if (values.length === this.variables.length) {
+      if (sumOfValues !== this.sum) {
+        return false
+      }
+    } else {
+      if (sumOfValues > this.sum) {
+        return false
+      }
+    }
+    return true
+  }
+}
+
 // No 4203
 const GRID_SHAPE = [
   'XXXXXXXXXX',
@@ -60,7 +92,34 @@ const DOWN_CLUES = [
   [7, 5, 8]
 ]
 
+const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// [ { variables: ['row:col', 'row:col', ...], sum: number } ]
+const findAcrossRuns = () => {
+  return []
+}
+
+// [ { variables: ['row:col', 'row:col', ...], sum: number } ]
+const findDownRuns = () => {
+  return []
+}
+
 const main = () => {
+  const acrossRuns = findAcrossRuns()
+  const downRuns = findDownRuns()
+  const variables = Array.from(new Set(acrossRuns.concat[downRuns].flatMap(({ variables }) => variables)))
+  const domains = new Map(variables.map(variable => [variable, DIGITS]))
+  const csp = new CSP(variables, domains)
+  const acrossConstraints = acrossRuns.map(({ variables, sum }) => new KakuroConstraint(variables, sum))
+  for (const constraint of acrossConstraints) {
+    csp.addConstraint(constraint)
+  }
+  const downConstraints = downRuns.map(({ variables, sum }) => new KakuroConstraint(variables, sum))
+  for (const constraint of downConstraints) {
+    csp.addConstraint(constraint)
+  }
+  const solution = csp.backtrackingSearch()
+  console.dir(solution)
 }
 
 main()
